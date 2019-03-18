20167f9eb7f5a40f8c4a06d1782656219c69fad1
Ahmad Fatayri <ahmadfatayri@Houssams-MacBook-Pro.local>
2019-03-18 15:41:18 +0200

    New project

---
+++ goalpost-app/goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Ahmad Fatayri on 3/18/19.
+//  Copyright © 2019 Ahmad Fatayri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
